f824622b1c6e2f6f49aea4fd81e5d282a8eac78b
Mateus Azis <lordx2@gmail.com>
2013-04-07 21:46:33 -0300

    Binary search methods.

--- src/store_reference/common.js
+++ src/store_reference/common.js
@@ -1,2 +1,35 @@
 ﻿// Scripts in this file are included in both the IDE and runtime, so you only
 // need to write scripts common to both once.
+function binaryUIDSearch(instance, array){
+	var start = 0, end = array.length - 1;
+	var center;
+	var searchedUID = instance["uid"];
+	while(start <= end){
+		center = Math.floor((start + end) / 2);
+		var current = array[center];
+		if(current["uid"] == searchedUID)
+			return {found:true, pos:center};
+		if(current["uid"] > searchedUID)
+			end = center - 1;
+		else
+			start = center + 1;
+	}
+	return {found:false, pos:center};
+}
+
+// var arr = [];
+// for(var i = 0; i < 10; i++)
+//     arr[i] = {uid:i*3};
+    
+// var log = console.log;
+// log("initial array: ", arr);
+// searched = {uid: 25};
+// var result = binaryUIDSearch(searched,arr);
+// log(result);
+// if(!result.found){
+//     log("previous length", arr.length);
+//     arr.splice(result.pos, 0, searched);
+//     log("adding");
+//     log("post length", arr.length);
+// }
+// log(arr);
