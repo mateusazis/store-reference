9c09a9377f469738a2042b09322f5adebc3e4b7d
Mateus Azis <lordx2@gmail.com>
2013-04-09 10:50:34 -0300

    Organized tests. Small improvement on binary search.

--- src/store_reference/common.js
+++ src/store_reference/common.js
@@ -1,6 +1,9 @@
 ﻿// Scripts in this file are included in both the IDE and runtime, so you only
 // need to write scripts common to both once.
 function binaryUIDSearch(instance, array){
+	if(array.length == 0)
+		return {found:false, pos:0};
+
 	var start = 0, end = array.length - 1;
 	var center = Math.floor((start + end) / 2);
 	var searchedUID = instance["uid"];
@@ -16,20 +19,3 @@ function binaryUIDSearch(instance, array){
 	}
 	return {found:false, pos:center};
 }
-
-// var arr = [];
-// for(var i = 0; i < 10; i++)
-//     arr[i] = {uid:i*3};
-    
-// var log = console.log;
-// log("initial array: ", arr);
-// searched = {uid: 25};
-// var result = binaryUIDSearch(searched,arr);
-// log(result);
-// if(!result.found){
-//     log("previous length", arr.length);
-//     arr.splice(result.pos, 0, searched);
-//     log("adding");
-//     log("post length", arr.length);
-// }
-// log(arr);
